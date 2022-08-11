be763f3d0c503dfc22dd49489d19c137f1537c22
Support <Support@WEMA-TRAINING-S>
2022-08-11 09:47:02 -0700

    Add project files.

---
+++ open and close coffee/Coffee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace open_and_close_coffee
+{
+    /// <summary>
+    /// this is the base class that other classes will inherit from
+    /// </summary>
+    public abstract class Coffee
+        {
+         public abstract string ServeCoffee();
+        }
+    
+   
+    /// <summary>
+    /// this is a derived class that extend the base class
+    /// it serves filterd coffee
+    /// </summary>
+    public class FilteredCoffee : Coffee
+        {
+       public override string ServeCoffee()
+        {
+            return "Serving Filtered Coffee";
+        }
+        }
+
+
+    /// <summary>
+    /// this is a derived class that extend the base class
+    /// it serves unfiltered coffee
+    /// </summary>
+    public class Unfilteredcoffee : Coffee
+        {
+        public override string ServeCoffee()
+        {
+            return "Serving Unfiltered Coffee";
+        }
+    }
+}
+   
---
+++ open and close coffee/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace open_and_close_coffee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Ayobola Coffee World\n");
+            Console.WriteLine("What type of coffee do you want\n");
+            Console.WriteLine("Enter 1 for Filtered Coffee\n");
+            Console.WriteLine("Enter 2 for Unfiltered Coffee\n");
+            //it catches all extensions
+            try
+            {
+                int coffeeType = int.Parse(Console.ReadLine());
+                //instantiation of classes
+                var filtered = new FilteredCoffee();
+                var unfiltered = new Unfilteredcoffee();
+
+                if(coffeeType == 1)
+                {
+                    Console.WriteLine(filtered.ServeCoffee()); 
+                }
+                else if(coffeeType == 2)
+                {
+                    Console.WriteLine(unfiltered.ServeCoffee()); 
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Invalid Input");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
